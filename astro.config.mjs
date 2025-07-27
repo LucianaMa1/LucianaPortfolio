b26741d9565fcefa33aadc064b3cc312f527a727
LucianaMa1 <110077217+LucianaMa1@users.noreply.github.com>
2025-07-27 16:52:52 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"; 
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   site: 'https://LucianaMa1.github.io',
   base: '/LucianaPortfolio/',
+  integrations: [tailwind()]    
 });
